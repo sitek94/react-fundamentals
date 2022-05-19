642e43878bcce86daef5a3f357ccb62e1ec93aac
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-05-19 06:13:07 +0200

    ex 5 - encapsulate size styling using size prop

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,10 +14,10 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-function Box({className, style, children}) {
+function Box({size, style, children}) {
   return (
     <div
-      className={['box', className].join(' ')}
+      className={`box box--${size}`}
       style={{
         fontStyle: 'italic',
         ...style,
@@ -31,13 +31,13 @@ function Box({className, style, children}) {
 function App() {
   return (
     <div>
-      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
         small lightblue box
       </Box>
-      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
         medium pink box
       </Box>
-      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
         large orange box
       </Box>
     </div>
